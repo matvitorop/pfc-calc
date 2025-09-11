8e69de516672c86e911d3ae26581dd486e3c02d1
MATVII <mattor264@gmail.com>
2025-09-11 23:49:18 +0300

    Add notes model and repo

--- project-server/Models-part/Notes.cs
+++ project-server/Models-part/Notes.cs
@@ -2,5 +2,11 @@
 {
     public class Notes
     {
+        public int Id { get; set; }
+        public int User_Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime Due_date { get; set; }
+        public DateTime Is_completed { get; set; }
+        public DateTime Completed_date { get; set; }
     }
 }
---
+++ project-server/Repositories-part/INotesRepository.cs
@@ -0,0 +1,13 @@
+﻿using project_server.Models_part;
+
+namespace project_server.Repositories_part
+{
+    public interface INotesRepository
+    {
+        Task<Notes> AddNoteAsync(int userId, string title, DateTime dueDate);
+        Task<Notes?> DeleteNoteAsync(int noteId);
+        Task<Notes?> UpdateNoteAsync(int noteId);
+        Task<IEnumerable<Notes>> GetCompletedNotesAsync(int userId);
+        Task<IEnumerable<Notes>> GetActiveNotesAsync(int userId);
+    }
+}
