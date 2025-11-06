e97b3dd289ab20d0ecdee7d4be5f418837c80745
MATVII <mattor264@gmail.com>
2025-11-07 00:49:12 +0200

    Add test version of search field

--- project-frontend/src/App.tsx
+++ project-frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import RegisterForm from './components/User/RegisterForm'
 import LoginForm from './components/User/LoginForm'
+import SearchItem from './components/Items/SearchItem'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <>
-            <RegisterForm />  
+            <SearchItem />  
         </>
   )
 }
